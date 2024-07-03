736cb91aeefa8c7b3e504184d4a273a116d218dd
Bharath <bharath19h142@gmail.com>
2024-07-03 09:58:24 -0700

    uploading files

--- src/utils/ApiService.jsx
+++ src/utils/ApiService.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from "axios";
 const API_URL = "https://6597a32d668d248edf2318e6.mockapi.io/api/v1/";
 const AxiosService = axios.create({
@@ -8,15 +7,4 @@ const AxiosService = axios.create({
   },
 });
 
-=======
-import axios from "axios";
-const API_URL = "https://6597a32d668d248edf2318e6.mockapi.io/api/v1/";
-const AxiosService = axios.create({
-  baseURL: API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
->>>>>>> 493711c968de3acf53084053c23087dd1cb53565
 export default AxiosService;
